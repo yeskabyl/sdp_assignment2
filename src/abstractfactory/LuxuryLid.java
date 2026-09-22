348f750f452ae176a61c6755b8a82ad40dcdd572
merey <eskabyl08m@gmail.com>
2026-09-22 14:53:24 +0500

    add LuxuryLid implementation

---
+++ src/abstractfactory/LuxuryLid.java
@@ -0,0 +1,7 @@
+package abstractfactory;
+
+public class LuxuryLid implements Lid {
+    public void attach() {
+        System.out.println("Attaching secure, heat-resistant silicone lid.");
+    }
+}
